371964d4b371ad6aa15470b02860b43ec1c956b7
Rakesh Buddhiraju <nagarakesh4@gmail.com>
2015-04-23 21:42:20 -0700

    testing to commit to new name repo

--- app/src/main/java/materialtest/sanjose/venkata/materialtest/NavigationDrawerFragment.java
+++ app/src/main/java/materialtest/sanjose/venkata/materialtest/NavigationDrawerFragment.java
@@ -6,6 +6,7 @@ import android.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,8 +20,11 @@ public class NavigationDrawerFragment extends android.support.v4.app.Fragment {
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
 
+
+
     public NavigationDrawerFragment() {
         // Required empty public constructor
+        Log.i("","");
     }
 
 
@@ -34,7 +38,7 @@ public class NavigationDrawerFragment extends android.support.v4.app.Fragment {
 
     public void setUp(DrawerLayout drawerLayout, Toolbar toolbar) {
         mDrawerLayout = drawerLayout;
-        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close){
+        mDrawerToggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
