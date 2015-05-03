7dfc6b15093f0650bbdde2418532cf91ed57fdc6
Venkata <nagarakesh4@users.noreply.github.com>
2015-05-03 12:06:48 -0700

    Update Logger.java

--- app/src/main/java/materialtest/sanjose/venkata/Logging/Logger.java
+++ app/src/main/java/materialtest/sanjose/venkata/Logging/Logger.java
@@ -1,7 +1,27 @@
-package materialtest.sanjose.venkata.Logging;
+package materialtest.sanjose.venkata.logging;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by buddhira on 5/1/2015.
  */
 public class Logger {
+
+    public static void showLogInfo(String message) {
+        Log.i("this is logInfo", "" + message);
+    }
+
+    public static void showLogDebug(String message) {
+        Log.d("this is logDebug", "" + message);
+    }
+
+    public static void showLogError(String message) {
+        Log.e("this is logErr", "" + message);
+    }
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message + "", Toast.LENGTH_SHORT).show();
+    }
 }
